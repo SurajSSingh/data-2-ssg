83161c98add6a8328dbcf1f433320858941b4c60
SurajSSingh <22825536+SurajSSingh@users.noreply.github.com>
2023-09-15 23:12:26 -0700

    Add (Simple) Template Transformation

--- generator.mjs
+++ generator.mjs
@@ -9,19 +9,26 @@ import {
 } from "node:fs";
 import { basename, dirname, extname, join, resolve } from "node:path";
 
-/** @type {string}  */
-const NAME = "Simple SSG";
-/** @type {string}  */
-const VERSION = "0.1.0";
-
 /**
+ * Types
  * @typedef Flag
  * @type {object}
  * @property {string} long
  * @property {string} short
  * @property {string} description
  * @property {string | undefined} configName
+
+ * @typedef Config
+ * @type {object}
+ * @property {string} data_dir
+ * @property {string} output_dir
+ * @property {string} template_dir
+ * @property {string} static_dir
  */
+
+// CONSTANTS
+const NAME = "Simple SSG";
+const VERSION = "0.1.0";
 
 /** @type {Flag[]}  */
 const FLAG_LIST = [
@@ -53,7 +60,6 @@ const FLAG_LIST = [
   { long: "version", short: "v", description: "Print the current version" },
 ];
 
-/** @type {number}  */
 const maxOptionLength = Math.max(
   ...FLAG_LIST.map(({ long, short }) => long.length + short.length + 4),
 );
@@ -73,6 +79,56 @@ ${"version" + " ".repeat(maxOptionLength - 3) + "See --version option"}
 OPTIONS:
 ${FLAGS_STRING}
 `;
+
+const DEFAULT_DATA_DIR_NAME = "./";
+const DEFAULT_OUTPUT_DIR_NAME = "public";
+const DEFAULT_TEMPLATE_DIR_NAME = "template";
+const DEFAULT_STATIC_DIR_NAME = "static";
+
+// const TEMPLATE_REGEX_SELF_CLOSING = /<\s*template\b\s*.*?\s*(?<slot1>\bslot\s*=\s*"(?<name1>[0-9A-Za-z_-]*)")\s*.*?\s*\/\s*>/;
+// const TEMPLATE_REGEX_WITH_DEFAULT = /<\s*template\b\s*.*?\s*(?<slot2>\bslot\s*=\s*"(?<name2>[0-9A-Za-z_-]*)")\s*[^/]*?\s*>\s*(?<default>.*)\s*<\s*\/\s*template\s*>/;
+// const namedPaddedRegex = (name, regexArray, flags = "") => new RegExp(`(?<${name}>${paddedRegex(regexArray).source})`, flags);
+
+/**
+ * Construct a Regex from an array of Regex with padding (`s*`) between each item
+ * @param {RegExp[]} regexArray
+ * @param {string} flags
+ * @returns {RegExp}
+ */
+const paddedRegex = (regexArray, flags = "") =>
+  new RegExp(regexArray.map((rgx) => rgx.source).join("\s*"), flags);
+
+const TEMPLATE_REGEX_SELF_CLOSING = paddedRegex([
+  /</,
+  /template\b/,
+  /.*?/,
+  /\bslot/,
+  /=/,
+  /(['"])(?<name>[0-9A-Za-z_-]+)\1/,
+  /.*?/,
+  /\//,
+  />/,
+], "g");
+const TEMPLATE_REGEX_WITH_DEFAULT = paddedRegex([
+  /</,
+  /template\b/,
+  /.*?/,
+  /\bslot/,
+  /=/,
+  /(['"])(?<name>[0-9A-Za-z_-]+)\1/,
+  /.*?/,
+  />/,
+  /(?<default>.*)/,
+  /</,
+  /\//,
+  /template/,
+  />/,
+], "g");
+
+const TEMPLATE = new RegExp(
+  `${TEMPLATE_REGEX_SELF_CLOSING.source}|${TEMPLATE_REGEX_WITH_DEFAULT.source}`,
+  "g",
+);
 
 /** @type {string[]}  */
 let args = [];
@@ -97,15 +153,6 @@ if (typeof process !== "undefined") {
   // Unknown specific code
   throw "Unsupported Runtime: Must be either Node, Deno, or Bun";
 }
-
-/**
- * @typedef Config
- * @type {object}
- * @property {string} data_dir
- * @property {string} output_dir
- * @property {string} template_dir
- * @property {string} static_dir
- */
 
 /** @type {Config} */
 const DEFAULT_CONFIG = {
@@ -133,12 +180,34 @@ function handleFileError(error) {
 /**
  * Read a file, transform it, and then write result to output dir
  * @param {string} srcFilePath - source file path
+ * @param {string} templatePath - template directory path
  * @returns {string | undefined}
  */
-function transformFile(srcFilePath) {
+function transformFile(srcFilePath, templatePath) {
   try {
-    const data = readFileSync(srcFilePath);
-    return data;
+    const data = readFileSync(srcFilePath).toString();
+    const templates = [...data.matchAll(TEMPLATE)];
+    let transformedData = "";
+    // Run transformation
+    if(templates.length > 0){
+        let start = 0;
+        for (const match of templates) {
+            transformedData += data.slice(start, match.index);
+            // Copy template there if it exists
+            const templateFile = join(templatePath, `${match.groups.name}.html`);
+            if(match.groups.name && existsSync(templateFile)){
+                transformedData+=readFileSync(templateFile).toString();
+            }
+            // Or just give default
+            else {
+                transformedData += match.groups.default ?? "";
+            }
+            start = match.index + match[0].length;
+        }
+        transformedData += data.slice(start);
+    }
+
+    return transformedData || data;
   } catch (error) {
     handleFileError(error);
   }
@@ -173,17 +242,17 @@ function processConfig(config) {
         : join(outputDir, outFileDir, ...names, "index.html");
 
       // Transform the file
-      const data = transformFile(filePath);
+      const data = transformFile(filePath, config.template_dir);
 
       // Write data to file
       if (data) {
-        // Create the output directory if it doesn't exist
-        if (!existsSync(dirname(outFilePath))) {
-          mkdirSync(dirname(outFilePath), { recursive: true });
-        }
+        // // Create the output directory if it doesn't exist
+        // if (!existsSync(dirname(outFilePath))) {
+        //   mkdirSync(dirname(outFilePath), { recursive: true });
+        // }
 
-        // Write the file to the output directory
-        writeFileSync(outFilePath, data);
+        // // Write the file to the output directory
+        // writeFileSync(outFilePath, data);
         console.log(`Converted and wrote ${filename} to ${outFilePath}`);
       } else {
         console.log(`No data from ${filename}`);
@@ -252,13 +321,22 @@ function readFlags(args) {
  * @returns {Config}
  */
 function getConfig(args) {
-  const config = DEFAULT_CONFIG;
+  let config = DEFAULT_CONFIG;
+  config = {
+    data_dir: DEFAULT_DATA_DIR_NAME,
+  };
+  config.output_dir = join(config.data_dir, DEFAULT_OUTPUT_DIR_NAME);
+  config.template_dir = join(config.data_dir, DEFAULT_TEMPLATE_DIR_NAME);
+  config.static_dir = join(config.data_dir, DEFAULT_STATIC_DIR_NAME);
   if (args) {
     let flags = readFlags(args);
     config.data_dir = flags.data_dir || config.data_dir;
-    config.output_dir = flags.output_dir || config.output_dir;
-    config.template_dir = flags.template_dir || config.template_dir;
-    config.static_dir = flags.static_dir || config.static_dir;
+    config.output_dir = flags.output_dir ||
+      join(config.data_dir, DEFAULT_OUTPUT_DIR_NAME);
+    config.template_dir = flags.template_dir ||
+      join(config.data_dir, DEFAULT_TEMPLATE_DIR_NAME);
+    config.static_dir = flags.static_dir ||
+      join(config.data_dir, DEFAULT_STATIC_DIR_NAME);
   }
   return config;
 }
@@ -276,7 +354,6 @@ function includesAny(arr, options) {
   return false;
 }
 
-// console.log(args);
 if (args[0] === "help" || includesAny(args, ["help", "--help", "-h"])) {
   console.log(`${HELP}`);
 } else if (
@@ -285,6 +362,5 @@ if (args[0] === "help" || includesAny(args, ["help", "--help", "-h"])) {
   console.log(`${NAME} ${VERSION}`);
 } else {
   const config = getConfig(args);
-  console.log(config);
-    processConfig(config);
+  processConfig(config);
 }
